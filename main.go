b9e4577b44567a4f74cc5b4b9f37404516aecbcc
Rusydy <rusydymuhiddin@gmail.com>
2020-09-13 05:56:12 +0700

    Try to 'parsing XML' file but I failed to get their elements of HTML

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Podcast_App/feeds"
 	"Podcast_App/itunes"
 	"log"
 )
@@ -19,6 +20,22 @@ func main() {
 		log.Printf("Artist: %s", item.ArtistName)
 		log.Printf("Podcast Name: %s", item.TrackName)
 		log.Printf("Feed url: %s", item.FeedURL)
+
+		feed, err := feeds.GetFeed(item.FeedURL)
+
+		if err != nil {
+			log.Fatalf("error while get feed: %v", err)
+		}
+
+		for _, podcast := range feed.Channel.Item {
+			log.Println("------------------")
+			log.Printf("Title: %s", podcast.Title)
+			log.Printf("Duration: %s", podcast.Duration)
+			log.Printf("Description: %s", podcast.Description)
+			log.Printf("URL: %s", podcast.Enclosure.URL)
+			log.Println("------------------")
+		}
+
 		log.Println("------------------")
 	}
 }
